e9d1eb8c1bb974a0ec06abdf771a8ed7ff87cbe3
George Akor <georgejupiter303@gmail.com>
2023-08-22 18:33:15 +0100

    Finally fixed the gradient issue. Seems i can't use variables in my css. maybe I used them wrong. Learned quite a bit about linear-gradient()

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import mobile from './images/illustration-woman-online-mobile.svg';
 import desktop from './images/illustration-woman-online-desktop.svg';
 import Question from './Question';
 
+
 function App() {
 
   const [questions] = useState(faq);
 
   return (
-    <div className="App" >
+    <div className="App">
+      <div className="container" >
       <article>
         <picture>
           <source media="(min-width: 768px)" srcSet={desktop} />
@@ -18,11 +20,14 @@ function App() {
         </picture>
       </article>
       <article>
-        <h1>FaQ</h1>
+        <h1>FAQ</h1>
         {questions.map((question) => {return <Question key={question.id} question={question.question} answer={question.answer} />})}
       </article>
 
     </div>
+
+    </div>
+    
   
   );
 }
